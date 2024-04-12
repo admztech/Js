3c02555788803ecbfb9629c0576200d6adc98a4f
Adnan <119958192+admztech@users.noreply.github.com>
2024-04-12 05:02:50 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,26 @@
+/*ECMA that is organization  ECMAScript defined the standard for javascript. */
+
+let name = "Hites"; // string datatype
+let age = 3; //number
+let isTrue = true // boolean data types
+
+//DataTypes (basic data types or build in primitive datatypes)
+/*
+1. number => 2 the power 53 range of the number (that is just the range as your memory address,
+that value is not fit into that range we use the biging data type).s
+
+2. bigint that is number data type that is not mostly use but can used like trading webiste  or stockmarket types website.
+
+3. String => '' " " ``;
+
+4. boolean => true/fals;
+
+5. null =>null that is standalone value (null is representation of a empty value.define is null value)
+
+6.undefine => (we have declear the variable but we don't define the value).
+
+7. symbol =>unique (that is  mostl use to fine the uniqueness of componest )*/
+
+//objects 
+console.log(typeof null);   // typeof null  data is object.
+console.log(typeof undefined); // type of the undefined datatye is undefined 
