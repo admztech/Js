5ae068663787f2bad824ddc67a173355d9b831c1
Adnan <119958192+admztech@users.noreply.github.com>
2024-04-12 04:18:01 +0000

    basic variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "hitesh@gamil.accountEmail";
+var anccountPassword = "123456";
+accountCity = "Lahore";
+// acountId =2; //can not change the value of const varible that is not allow;
+accountEmail = "hc@hc.com";
+anccountPassword = "654321"
+accountCity="Multan"; 
+//we can declear the varible without writing the key word befor it but we not use that method.
+
+/*
+prefor not to use the var ketword 
+becase to block and fucntion socpe issues with var.
+*/
+console.table([accountId, accountEmail,accountCity,anccountPassword]);
