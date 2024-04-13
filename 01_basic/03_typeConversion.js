e6cb36242ee7126d0f2d4f3af18215107e9edf0f
Adnan <119958192+admztech@users.noreply.github.com>
2024-04-13 05:35:42 +0000

    string covnerstion

--- 01_basic/03_typeConversion.js
+++ 01_basic/03_typeConversion.js
@@ -10,7 +10,7 @@ let valueInNumber = Number(score);
 
 let  isloggIn = 123;
 
-let boolenIsloggIn = Boolean(isloggIn)
+// let boolenIsloggIn = Boolean(isloggIn)
 //console.log(boolenIsloggIn);
 
 //boolean covnerstion
@@ -19,3 +19,23 @@ let boolenIsloggIn = Boolean(isloggIn)
 //""=>false
 // "asdfas"=>true
 // null =>false
+//**********Operations**********//
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+//Basic operation that is well know
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+let str1 ="hello"
+let str2 = " Adnan"
+let str3 = str1 + str2;
+// console.log(str3);
+// console.log("2"+2);
+// console.log(2+"2");
+// console.log(1+2+"3");
+
+//console.log(+true)  => 1 that is tricky way to type conversion or there output is 1
+// console.log (+"") =>0 that is also a tricky way to conversion so don't use them 
