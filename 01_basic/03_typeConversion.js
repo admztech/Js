8aaccc2aecd916626c80f2d52662bab4931176b4
Adnan <119958192+admztech@users.noreply.github.com>
2024-04-13 03:36:58 +0000

    conversion

---
+++ 01_basic/03_typeConversion.js
@@ -0,0 +1,21 @@
+let score = "33as";
+// console.log(typeof(score));
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" =>33
+//"33abc" => NaN
+//true => 1; false=>0;
+
+let  isloggIn = 123;
+
+let boolenIsloggIn = Boolean(isloggIn)
+//console.log(boolenIsloggIn);
+
+//boolean covnerstion
+// 1=> true
+//0=> false
+//""=>false
+// "asdfas"=>true
+// null =>false
