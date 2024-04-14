5c500c11091eb47790346c762d63d52101b38727
Adnan <119958192+admztech@users.noreply.github.com>
2024-04-14 04:42:21 +0000

    modern way to write string in Js

---
+++ 05_string.js
@@ -0,0 +1,3 @@
+const name= "Adnan";
+/* the morder way to write the string using the backticks , that make easy to inject the string using dollar sign and make more readable */
+console.log(`Hello my name is ${name}`);
