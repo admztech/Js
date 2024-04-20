933412c54f40415328a26da350850d638b36120f
Adnan <119958192+admztech@users.noreply.github.com>
2024-04-20 07:20:42 +0000

    function

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -1,2 +1,30 @@
 /* unction is the package or block of code in which that cantain the number of lines code.
 we can use that block or package of code where to by calling the function*/
+
+function sayMyName(){
+    console.log("A");
+    console.log("d");
+    console.log("n");
+    console.log("a");
+    console.log("n");
+}
+sayMyName  // that is called the function reference when we write function name without parentheses.
+// sayMyName() //that function is execute now .becacse we we write the function with it parentheses.
+
+// function addTwoNumber (number1,number2){
+// console.log(number1+number2);
+// }
+// const result = addTwoNumbers(3,5);
+// /* the inside the result is undefined becasue the that function resturn nothing console
+// .log use for print the out put.we need to return .used retunr keu*/
+// console.log("Result :",result);  
+
+function addTwoNumbers(number1,number2){
+    let result = number1 + number2;
+    return result;
+}
+// console.log(addTwoNumbers(4,8));
+
+function loginUserName(username){
+    
+}
