a0ebb976d2bc8c032d61afce82052c37366b64a7
Adnan <119958192+admztech@users.noreply.github.com>
2024-04-23 08:08:56 +0000

    Control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,1 +1,23 @@
-// if
+// code can run on conditional base that is called control follow and logic follow 
+// <,>,<=,>=,==,===,!=,!==
+// const teamprature =41;
+// if (teamprature===41){
+//     console.log("teamprature is less than 50");
+// }else
+// {console.log("teamprature is greater than 51"); }     
+
+/* short-hand notation */
+// const balance=1000;
+// if(balance > 500) console.log("test");  // if you single line then use the single line short hand notation.
+const userLogin = true;
+const debitCard = true;
+const logdedInFromGoogle = false;
+const logdedInFromEmai = true;
+
+if(userLogin && debitCard){
+    console.log("Allow user to shoping");
+}
+
+if(logdedInFromEmai || logdedInFromGoogle){
+    console.log("User logged in");
+}
