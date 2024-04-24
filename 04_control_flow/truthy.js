a99439398e07e3a01366593fb79a51074eee26d9
Adnan <119958192+admztech@users.noreply.github.com>
2024-04-24 07:45:21 +0500

    Update truthy.js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -19,7 +19,9 @@ if(Object.keys(myObject).length===0){
     console.log("object is Empty");
 }
 
-//Nullish CoalescingOperator (??) : null undefined
+/*Nullish CoalescingOperator (??) : null undefined(When we call data from databse or expecially from firebase etc)
+we can't get directly responsed. we get two value null response or not get a respone means undefined. for this
+special case to make the code secure we use the nullish coalesing operater*/
 
 let val1 ;
 // val1 = null??10;
