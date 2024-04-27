241600fb2af7665ffa91789f31b21460effd297d
Adnan <119958192+admztech@users.noreply.github.com>
2024-04-27 16:25:24 +0000

    iteration

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -1,5 +1,10 @@
 /* for-of loop: that loops use for array, string , map or javascript object*/ 
 const myArray =[1,2,3,4,5];
 for (const elements of myArray) {
-    console.log(elements);
+    // console.log(elements);
+}
+
+const greeting = "Hello World";
+for (const greet of greeting){
+    console.log(`Each char is ${greet}`);
 }
