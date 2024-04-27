a399ec343a177bf011acd37165a57eb2e8348002
Adnan <119958192+admztech@users.noreply.github.com>
2024-04-27 14:32:39 +0000

    for loop

--- 05_iteration.js/three.js
+++
@@ -1,5 +0,0 @@
-/* for-of loop*/
-const myArray =[1,2,3,4,5];
-for (const num of object) {
-    
-}
---
+++ 05_iteration/three.js
@@ -0,0 +1,5 @@
+/* for-of loop: that loops use for array, string , map or javascript object*/ 
+const myArray =[1,2,3,4,5];
+for (const elements of myArray) {
+    console.log(elements);
+}
