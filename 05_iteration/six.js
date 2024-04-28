223f09281cf6d0f0733bf86cc9dff252c5de95ab
Adnan <119958192+admztech@users.noreply.github.com>
2024-04-28 11:29:44 +0000

    filter

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -5,10 +5,19 @@
 const values = coding.forEach( (item)=>{ return (item);})
 console.log(values);*/
 
-const myNums = [1,2,3,4,5,6,7,8,9,10] 
 
 // filter properies use to return the values.
 
-const newNums = myNums.filter( (num) =>  { return num > 4});
+// const newNums = myNums.filter( (num) =>  { return num > 4});
 
+/* we can also use the foreach to retunr the values but we need to logic */
+const myNums = [1,2,3,4,5,6,7,8,9,10] 
+
+const newNums = [];
+
+myNums.forEach( (num) => {
+    if(num > 5 ){
+        newNums.push(num);
+    }
+})
 console.log(newNums);
